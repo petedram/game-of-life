2118637786490efeafd09d5930ee293b975aba8c
PeteDram <me@petejames.com>
2020-10-14 19:54:26 -0700

    virtual env setup + Cell/Grid classes in pseudo

---
+++ index.py
@@ -0,0 +1,27 @@
+'''
+Cell class with:
+- status of alive or dead
+- method to set alive
+- method to set dead
+- method to check status
+
+
+'''
+
+class Cell:
+    pass
+
+
+
+'''
+grid class with:
+
+- ability set the rows and columns
+- a method to draw out the grid
+- a method to check the neighbours of each cell
+- a method to update the board based on status of each cell in the next generation
+
+'''
+
+class Grid:
+    pass
