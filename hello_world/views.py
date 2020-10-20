ba9b70523c2e5f3282b6dc06660899f10e1fec40
PeteDram <me@petejames.com>
2020-10-19 21:03:22 -0700

    django base app

---
+++ hello_world/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def hello_world(request):
+    return render(request, 'hello_world.html', {})
+
+
+def downloadexcel(request):
+    print('yuuup')
+    return HttpResponseRedirect('/')
